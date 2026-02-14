043267590d0a6c9f3078479fb3049d3c0c1e41b3
Filamss <117148654+Filamss@users.noreply.github.com>
2026-02-15 00:59:10 +0700

    feat: Hidden testimoni

--- app/page.tsx
+++ app/page.tsx
@@ -189,7 +189,7 @@ export default function Home() {
       </section>
 
       <Steps />
-      <Testimonials />
+      {/* Testimonials temporarily hidden */}
       <LocationHours />
       <OrderSection
         cart={cart}
